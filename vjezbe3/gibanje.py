168b4c0fd146e72ec82064aa27ebe998d4ee04a0
fzilic1 <fzilic@pmfst.hr>
2022-03-18 20:09:46 +0100

    vjezbe 3

--- vjezbe3/gibanje.py
+++ vjezbe3/gibanje.py
@@ -2,14 +2,18 @@ import particle as prt
 import numpy as np
 
 p1=prt.Particle()
-p1.set_initial_conditions(5, 30, 2, 0, 0.01)
+v0=5
+alpha=30
+x0=3
+y0=10
+dt=0.01
 
-D_ana=((5**2)*np.sin(2*30*np.pi/180)/9.81)+2
+p1.set_initial_conditions(v0, alpha, x0, y0, dt)
+
+D_ana = (v0 * np.cos(alpha*np.pi/180) * (v0 * np.sin(alpha*np.pi/180) + np.sqrt((v0 * np.sin(alpha*np.pi/180))**2 + 2 * 9.81 * y0)) / 9.81)+x0
 D_num=p1.range()
+
 print (D_ana)
 print (D_num)
 Err=abs(D_ana-D_num)/D_ana
-print (Err)
-
-p1.reset()
-p1.set_initial_conditions(5, 30, 2, 0, 0.01)
+print(Err)
--- vjezbe3/zd32.py
+++ vjezbe3/zd32.py
@@ -4,19 +4,21 @@ import matplotlib.pyplot as plt
 
 v0=10
 alpha=60
-dt=[0]
-Err=[0]
+dt=[0.0001]
+
+Err=[]
 
 p=prt.Particle()
 
-for i in range(10):
+for i in range(1000):
     p.set_initial_conditions(v0, alpha, 0, 0, dt[i])
-    D_ana=((v0**2)*np.sin(2*alpha*np.pi/180)/9.81)
+    D_ana = v0**2*np.sin(2*alpha*np.pi/180)/9.81
     D_num=p.range()
-    dt.append(dt[-1]+0.01)
-    Err.append[100*abs(D_ana-D_num)/D_ana]
+    Err.append(100*abs(D_ana-D_num)/D_ana)
+    dt.append(dt[-1]+0.0001)
     p.reset()
 
+del dt[-1]
 plt.plot(dt, Err)
 plt.xlabel("dt [s]")
 plt.ylabel("Relativna pogreška [%]")
