f64c52ff49d3071288b4d83d29017a09a8500261
fzilic1 <fzilic@pmfst.hr>
2022-04-14 09:45:36 +0200

    vjezbe 6

--- vjezbe6/zd63.cpp
+++ vjezbe6/zd63.cpp
@@ -1,16 +1,33 @@
 #include <iostream>
+#include <list>
 
-void interval(int list[], int a, int b){
-    int s=list.size();
-    for(int i=0; i<=s; i++){
+void interval(int list[], int size, int a, int b){
+    for(int i=0; i<size; i++){
         if (list[i] >= a && list[i] <= b){
-            std::cout << list[i] <<std::endl;
+            std::cout << list[i] << " " ;
         }
     }
 }
 
+void replace(int list[], int size, int p1, int p2){
+    int a=list[p1];
+    int b=list[p2];
+    list[p1]=b;
+    list[p2]=a;
+    }
+
+void reverse(int list[], int size){
+    for(int i=0; i<size; i++){
+        replace(list, size, i, size-i);
+    }
+    for(int i=0; i<size; i++){
+        std::cout << list[i] << " " ;
+}}
+
 int main(){
     int lista[6]={4, 0, 5, 2, -1, 9};
-    interval(lista, 1, 8);
+    //interval(lista, 6, 1, 8);
+    //replace(lista, 6, 1, 4);
+    reverse(lista, 6);
     return 0;
 }
