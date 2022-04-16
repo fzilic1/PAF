1b69298f7cf40385faaebe10f3ba0335c5bf753c
fzilic1 <fzilic@pmfst.hr>
2022-04-16 21:05:07 +0200

    dz 2

--- dz/dz21.py
+++ dz/dz21.py
@@ -1,4 +1,26 @@
 import ngibanje as ng
+import matplotlib.pyplot as plt
 
-p=ng.pos(0, 0, 2, 5, 0.01)
-print (p)
+t=0.1
+T=[]
+X=[]
+while t<12:
+    T.append(t)
+    X.append(ng.pos(2, 0, 2, t, 0.0001, 0, -5, 0, 0))
+    t+=0.1
+plt.plot(T, X)
+plt.xlabel('t [s]')
+plt.ylabel('x [s]')
+plt.show()
+
+t=0.1
+T=[]
+V=[]
+while t<12:
+    T.append(t)
+    V.append(ng.vel(0, 0, 2, t, 0.0001, 0, 0, 0, 6))
+    t+=0.1
+plt.plot(T, V)
+plt.xlabel('t [s]')
+plt.ylabel('v [m/s]')
+plt.show()
--- dz/ngibanje.py
+++ dz/ngibanje.py
@@ -1,33 +1,33 @@
-def pos(x_0, v_0, m, t, dt):
+def pos(x_0, v_0, m, t, dt, n1, n2, n3, n4):
     T=0
     x=x_0
     v=v_0
     while T <= t:
-        F=T**2+2*v+x
+        F=n1*v+n2*x+n3*t+n4
         a=F/m
         x+=v*dt
         v+=a*dt
         T+=dt
     return x
 
-def vel(x_0, v_0, m, t, dt):
+def vel(x_0, v_0, m, t, dt, n1, n2, n3, n4):
     T=0
     x=x_0
     v=v_0
     while T <= t:
-        F=T**2+2*v+x
+        F=n1*v+n2*x+n3*t+n4
         a=F/m
         x+=v*dt
         v+=a*dt
         T+=dt
     return v
 
-def acc(x_0, v_0, m, t, dt):
+def acc(x_0, v_0, m, t, dt, n1, n2, n3, n4):
     T=0
     x=x_0
     v=v_0
     while T <= t:
-        F=T**2+2*v+x
+        F=n1*v+n2*x+n3*t+n4
         a=F/m
         x+=v*dt
         v+=a*dt
