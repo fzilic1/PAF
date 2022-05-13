7136089eb7be2dbcd99b3dfe148a09597970860b
fzilic1 <fzilic@pmfst.hr>
2022-05-13 12:23:16 +0200

    vjezbe 8

--- vjezbe8/zd81.py
+++ vjezbe8/zd81.py
@@ -1,4 +1,5 @@
 import Projectile as pr
+import matplotlib.pyplot as plt
 
 p1=pr.Projectile()
 p2=pr.Projectile()
@@ -7,3 +8,4 @@ p2.set_initial_conditions(20, 45, 0, 0, 2, 1.225, 0, 0.5 )
 
 p1.plot_trajectory()
 p2.plot_trajectory()
+plt.show()
